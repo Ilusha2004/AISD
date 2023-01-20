02edcdd3932f3af4de486620f31d3325cdb79c10
KoVLya <speedbreaker228@gmail.com>
2023-01-20 16:36:15 +0300

    0.4 add

---
+++ DataStructure/0.4. Канонический вид (по матрице смежности)/0.4.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    ofstream out("output.txt");
+    ifstream in("input.txt");
+
+    int n;
+
+    in >> n;
+
+    map<int,int> kanon;
+
+    int value;
+
+    for(int i = 0; i < n; i++){
+        for(int k = 0; k < n; k++){
+            in >> value;
+            if(value == 1){
+                kanon[k + 1] = i + 1;
+            }
+        }
+    }
+
+
+    for(auto id : kanon){
+        cout << id.first << " " << id.second << endl;
+    }
+
+    int prev = 0;
+
+    for(auto id : kanon){
+        if((prev + 2 == id.first)){
+            out << 0 << " ";
+        }
+        out << id.second << " ";
+        prev = id.first;
+    }
+
+    if(prev == kanon.size()){
+        out << 0 << " ";
+    }
+}
